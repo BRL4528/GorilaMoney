54c82b419b34f23a7ee2df4fdf3560dd4accfbb2
brl4528 <blgc.sgo@hotmail.com>
2021-03-24 06:20:10 -0400

    create repo, initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionMOdal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,19 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionMOdal: () => void;
+}
+
+export function Header({ onOpenNewTransactionMOdal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
 
-        <button type="button">Nova Transação</button>
+        <button type="button" onClick={onOpenNewTransactionMOdal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
   );
